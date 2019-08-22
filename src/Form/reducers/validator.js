50efb3520dcfc0341f90fbd5785d628c65126396
Maximilly <devsnaked@localhost.localdomain>
2019-08-22 01:16:00 -0300

    applyValidators dispatch one map of erro

--- src/Form/reducers/validator.js
+++ src/Form/reducers/validator.js
@@ -2,9 +2,8 @@ import { Map } from 'immutable';
 
 export default function formReducer(state = Map({}), action) {
     switch (action.type) {
-        case "UPDATE_FIELD_MESSAGE":
-            const { field, message } = action
-            return state.set(field, message)
+        case "SET_ERR_MAP":
+            return action.map;
         default:
             return state;
     }
