a7d00e0cc7a5720b87e0e2940286331f03243bd8
Maximilly M <max.ocw@gmail.com>
2019-08-26 01:07:34 -0300

    function to push or remove item from list

--- src/Form/reducers/form.js
+++ src/Form/reducers/form.js
@@ -1,14 +1,31 @@
-import { Map } from 'immutable';
+import { Map, List } from 'immutable';
 
 export default function formReducer(state = Map({}), action) {
     switch (action.type) {
         case "UPDATE_FIELD":
-            const { field, value } = action
+            let { field, value } = action
             return state.set(field, value)
         case "SET_FORM_STATE":
-            const { data } = action
+            let { data } = action
             return new Map(data)
+        case 'UPDATE_FIELD_MULTI_SELECT':
+            return updateMultiSelectList(state, action)
         default:
             return state;
     }
+}
+
+
+function updateMultiSelectList(state, action) {
+    const { field, value } = action
+    if (!state.get(field))
+        return state.set(field, new List([value]))
+
+
+    return state.update(field, list => {
+        if (list.includes(value)) {
+            return list.filter(item => item !== value)
+        }
+        return list.push(value)
+    })
 }
