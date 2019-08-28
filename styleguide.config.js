6ac1ff997087b3a1057c771132d12b3206d61896
Maximilly M <max.ocw@gmail.com>
2019-08-28 16:30:13 -0300

    doc introduction

---
+++ styleguide.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  components: 'src/lib/**/[A-Z]*.js',
+  sections: [
+    {
+      name: 'Introduction',
+      content: './src/docs/introduction.md'
+    },
+    {
+      name: 'Usage',
+      content: './scr/docs/usage.md'
+    }
+  ]
+}
